f52ece799a83c262afd4e9b653a2c570f0a3cc50
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-03-07 09:50:09 +0100

    proptypes

--- app/index.js
+++ app/index.js
@@ -1,17 +1,22 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+const PropTypes = require('prop-types')
 require('./index.css')
 
 class App extends React.Component {
   render () {
     return ( <div>
-        Hello World!
+        Hello {this.props.name}!
       </div>
     )
   }
 }
 
+App.propTypes = {
+  name: PropTypes.string.isRequired,
+}
+
 ReactDOM.render(
-  <App />,
+  <App name="oskar"/>,
   document.getElementById('app')
 )
